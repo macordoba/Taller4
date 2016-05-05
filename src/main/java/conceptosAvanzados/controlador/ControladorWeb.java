2b95e382a7fd1a8dc965394bf7fa101c44f1406e
am.cordoba1@uniandes.edu.co <macordoba91@gmail.com>
2016-05-05 03:47:02 -0500

    v3

--- src/main/java/conceptosAvanzados/controlador/ControladorWeb.java
+++ src/main/java/conceptosAvanzados/controlador/ControladorWeb.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ControladorWeb extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
